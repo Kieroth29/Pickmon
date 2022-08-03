70ec85f7ba3e9ab38c0313fa3080aa986bcf238a
Marcos Vinícius Queiroz <49909597+Kieroth29@users.noreply.github.com>
2022-08-03 19:22:44 -0300

    Handling environment variables

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 import json
 from os import environ
+from dotenv import load_dotenv
 from flask import Flask, Response, request
 from flask_cors import CORS
 from modules.db import DB
 
 app = Flask(__name__)
 CORS(app)
+load_dotenv()
 
 @app.route('/', methods=['GET'])
 def home():
@@ -33,4 +35,4 @@ def random_pokemon():
     return {'pokedexId': pokemon['pokedex_id'], 'name': pokemon['name'], 'type': pokemon['type']}
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5051, ssl_context=(environ.get('SERVER_CERT'), environ.get('SERVER_KEY')))
+    app.run(host="0.0.0.0", port=5051, ssl_context=(environ.get('SERVER_CERT'), environ.get('SERVER_KEY')))
